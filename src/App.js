6e6c40206d6ffc66dc401573d7862166453ab49d
Khaleel Hamid <khaleel@lynk-me.com>
2023-04-19 17:04:40 -0400

    Removed all unecessary comments. Tidied up code.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import TitleCard from "./Components/TitleCard";
 import ItemSelector from "./Components/ItemSelector";
 
 function App() {
-  //This is jsx. ie: html code in javascript coutesy react.
   return (
     <div>
       <TitleCard/>
-      <ItemSelector/>
-      
+      <ItemSelector/> 
     </div>
   );
 }
